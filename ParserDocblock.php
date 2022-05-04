a8aef72fb64298aedbeec6c54688210edcb41ffd
Michael <m@cadot.eu>
2022-05-04 20:39:19 +0200

    start

---
+++ ParserDocblock.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+use ReflectionClass;
+
+class ParserDocblock
+{
+    private
+        $alias,
+        $Entity,
+        $reflexion,
+        $selects,
+        $options,
+        $types,
+        $properties,
+        $baseAlias;
+
+
+    public function __construct(string $entity = '', array $baseAlias = ['simple', 'choice', 'choiceenplace', 'entity', 'collection', 'color', 'password', 'hidden', 'image'])
+    {
+        $this->setEntity($entity);
+        $this->baseAlias = $baseAlias;
+        foreach ($this->reflexion->getProperties() as $property) {
+            $name = $this->getName($property);
+            $this->options[$name] = $this->parseOptions($property);
+            $this->alias[$name] = $alias = $this->getAlias($property);
+            $this->types[$name] = $type = $this->findType($property);
+            //on prend l'alias en priorité
+            $this->selects[$name] = $alias != '' ? $alias : $type;
+            $this->properties[$name] = $property;
+        }
+    }
+
+    /**
+     * It sets the entity and the reflection of the class.
+     * 
+     * @param string The name of the class that is being called.
+     */
+    public function setEntity($string): void
+    {
+        $this->Entity = ucfirst($string);
+        $class = 'App\Entity\\' . $this->Entity;
+        $this->reflexion = new \ReflectionClass(new $class);
+    }
+
+    /**
+     *  Returns the reflection of the class
+     * 
+     * @return \ReflectionClass The reflection class of the class that is being called.
+     */
+    public function getReflexion(): \ReflectionClass
+    {
+        return $this->reflexion;
+    }
+    public function getAttributes(string $name): array
+    {
+        return $this->reflexion->getProperty($name)->getAttributes();
+    }
+
+    public function getAttribute(string $name, $nameAttribute): \ReflectionAttribute
+    {
+        foreach ($this->getAttributes($name) as  $num => $attribute) {
+            if (is_string($nameAttribute) && $attribute->getName() == $nameAttribute) return $attribute;
+            if (is_integer($nameAttribute) && $num == $nameAttribute) return $attribute;
+        };
+    }
+    public function getArgumentsOfAttributes(string $name, $nameAttribute): array
+    {
+        return $this->getAttribute($name, $nameAttribute)->getArguments();
+    }
+    public function getArgumentOfAttributes(string $name, $nameAttribute, $nameArgument)
+    {
+        return $this->getAttribute($name, $nameAttribute)->getArguments()[$nameArgument];
+    }
+    public function getProperty($name): \ReflectionProperty
+    {
+        return $this->properties[$name];
+    }
+    public function getSelect($name): string
+    {
+        return $this->selects[$name];
+    }
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+    public function getType(string $name): string
+    {
+        return $this->types[$name];
+    }
+
+    /**
+     * It takes a property and returns an array of options
+     * 
+     * @param \ReflectionProperty property The property to be processed
+     */
+    private function parseOptions(\ReflectionProperty $property)
+    {
+        $options = [];
+        $docs = $property->getDocComment();
+        $tab = explode("\n", $docs);
+        foreach (explode("\n", $docs) as $doc) {
+            //si on a une valeur
+            if ($this->clean($doc) != '') {
+                //We look if we have an action and value
+                if (($deb = strpos($doc, ':')) === false) {
+                    $options[strtolower($this->clean(substr($doc, 0, $deb)))] = $this->clean($doc);
+                } else {
+                    $key = strtolower($this->clean(substr($doc, 0, $deb)));
+                    //merge or create
+                    if (isset($options[$key])) {
+                        //control presence of key and value
+                        if (is_array(json_decode(substr($doc, $deb + 1), true))) {
+                            $val = $this->jsondecode(substr($doc, $deb + 1), true);
+                        } else {
+                            $val = $this->jsondecode('{"' . substr($doc, $deb + 1) . '":""}', true);
+                        }
+                        $options[$key] =  array_merge($options[$key], $val);
+                    } else {
+                        //control presence of key and value
+                        if (is_array(json_decode(substr($doc, $deb + 1), true))) {
+                            $options[$key] = $this->jsondecode(substr($doc, $deb + 1), true);
+                        } else {
+                            $options[$key] = $this->jsondecode('{"' . substr($doc, $deb + 1) . '":""}', true);
+                        }
+                    }
+                }
+            }
+        }
+        return $options;
+    }
+    /**
+     * It takes a string, decodes it as JSON, and returns the decoded object
+     * 
+     * @param string The string being decoded.
+     * @param bool If this is true, then the object will be converted into an associative array.
+     * 
+     * @return the value of the variable .
+     */
+    public function jsondecode($string, $bool = false)
+    {
+        $json = json_decode($string,  $bool);
+        if ($json == null && $string != '') dd('!!erreur de syntaxe sur' . $string);
+        return $json;
+    }
+
+
+    /**
+     * It returns the type of the property
+     * 
+     * @param \ReflectionProperty property The property to be analyzed
+     * 
+     * @return string The type of the property.
+     */
+    public function findType(\ReflectionProperty $property): string
+    {
+        $tab = '';
+        foreach ($property->getAttributes() as $attr) {
+            $fin = strtolower(array_reverse(explode('\\', $attr->getName()))[0]);
+            if ($fin == 'column' && !$tab)
+                $tab = isset($attr) && isset($attr->getArguments()['type']) ? $attr->getArguments()['type'] : '';
+            if ($fin != 'column')
+                $tab = strtolower(array_reverse(explode('\\', $attr->getName()))[0]);
+        }
+        return $tab;
+    }
+
+    /**
+     * It removes all the characters in the second parameter from the first parameter
+     * 
+     * @param string The string to be cleaned.
+     * 
+     * @return string The string with all the whitespace removed.
+     */
+    public function clean($string): string
+    {
+        return trim($string, " \t\n\r\0\x0B*\/\\");
+    }
+    /**
+     * This function returns the name of the property
+     * 
+     * @param \ReflectionProperty property The property that is being serialized.
+     * 
+     * @return string The name of the property.
+     */
+    public function getName(\ReflectionProperty $property): string
+    {
+        return $property->getName();
+    }
+    /**
+     * It gets the alias of a property
+     * 
+     * @param \ReflectionProperty property The property to be processed
+     * 
+     * @return string The alias of the property.
+     */
+    private function getAlias(\ReflectionProperty $property): string
+    {
+        $docs = $property->getDocComment();
+        $tab = explode("\n", $docs);
+        if (isset($tab[1]) && strpos($tab[1], ':') === false && in_array($this->clean($tab[1]), $this->baseAlias)) {
+            return $this->clean($tab[1]);
+        }
+        return '';
+    }
+}
---
+++ ToolsHelper.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\ParametresRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Yaml\Yaml;
+
+class ToolsHelper
+{
+    /**
+     * Get the list of enabled locales from the translation.yaml file
+     *
+     * @return An array of enabled locales.
+     */
+    public static function get_langs()
+    {
+        $yaml = new Yaml();
+        if (file_exists('/app/config/packages/translation.yaml')) {
+            $trans = $yaml->parseFile('/app/config/packages/translation.yaml')['framework'];
+            if (isset($trans['enabled_locales'])) {
+                return $trans['enabled_locales'];
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * This function will return a random article from Wikipedia
+     *
+     * @return the content of the page.
+     */
+    public static function wikipedia_article_random()
+    {
+        if (! function_exists('curl_init')) {
+            return false;
+        }
+        $url = 'https://en.wikipedia.org/api/rest_v1/page/random/html';
+        $options = [
+            CURLOPT_RETURNTRANSFER => true,
+            // return web page
+            CURLOPT_HEADER => false,
+            // don't return headers
+            CURLOPT_FOLLOWLOCATION => true,
+            // follow redirects
+            CURLOPT_ENCODING => "",
+            // handle all encodings
+            CURLOPT_USERAGENT => "spider",
+            // who am i
+            CURLOPT_AUTOREFERER => true,
+            // set referer on redirect
+            CURLOPT_CONNECTTIMEOUT => 30,
+            // timeout on connect
+            CURLOPT_TIMEOUT => 30,
+            // timeout on response
+            CURLOPT_MAXREDIRS => 3,
+            // stop after 10 redirects
+        ];
+
+        $ch = curl_init($url);
+        curl_setopt_array($ch, $options);
+        $content = curl_exec($ch);
+        $err = curl_errno($ch);
+        $errmsg = curl_error($ch);
+        $header = curl_getinfo($ch);
+        curl_close($ch);
+        return $content;
+        if (preg_match('/<title>(.*?)<\/title>/i', $content, $matches)) {
+            $title = str_replace(' - Wikipedia, the free encyclopedia', '', $matches[1]);
+        }
+
+        return '<a href="' . $header['url'] . '">' . $title . '</a>';
+    }
+
+    /**
+     * It returns an array of all the parameters in the database
+     *
+     * @param EntityManagerInterface em The entity manager
+     *
+     * @return An array of all the parameters in the database.
+     */
+    public static function params(EntityManagerInterface $em)
+    {
+        $tab = [];
+        foreach ($em->getRepository('App:Parametres')->findAll() as $parametre) {
+            $tab[$parametre->getNom()] = $parametre->getValeur();
+        }
+        return $tab;
+    }
+}
