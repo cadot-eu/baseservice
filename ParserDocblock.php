913183326a4beb2e0cf4831bccd8468cee3a050f
Michael <m@cadot.eu>
2022-08-25 15:12:39 +0200

    nocrud et onytype

--- ParserDocblock.php
+++ ParserDocblock.php
@@ -18,7 +18,7 @@ class ParserDocblock
         $baseAlias;
 
 
-    public function __construct(string $entity = '', array $baseAlias = ['simple', 'simplelanguage', 'vide', 'normal', 'choice', 'choiceenplace', 'entity', 'collection', 'color', 'email', 'password', 'hidden', 'hiddenroot', 'readonlyroot', 'image', 'fichier', 'money', 'nocrud', 'telephone', 'siret', 'array', 'json', 'order', 'search'])
+    public function __construct(string $entity = '', array $baseAlias = ['simple', 'simplelanguage', 'vide', 'normal', 'choice', 'choiceenplace', 'entity', 'collection', 'color', 'email', 'password', 'hidden', 'hiddenroot', 'readonlyroot', 'image', 'fichier', 'money', 'telephone', 'siret', 'array', 'json', 'order', 'search'])
     {
         $this->setEntity($entity);
         $this->baseAlias = $baseAlias;
@@ -31,10 +31,6 @@ class ParserDocblock
             $this->selects[$name] = $alias != '' ? $alias : $type;
             $this->properties[$name] = $property;
         }
-        // on s'arrête dans le cas d'un nocrud
-        if ($this->alias['id'] == 'nocrud') {
-            dd('Protection par NOCRUD');
-        };
     }
 
     /**
