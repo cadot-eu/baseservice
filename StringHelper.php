b93f7ad7210500da5f75d5534e045f2d87f1ef54
Michael <m@cadot.eu>
2022-09-16 13:15:18 +0200

    resize picture

--- StringHelper.php
+++ StringHelper.php
@@ -251,28 +251,7 @@ class StringHelper
         return array_keys(array_slice($tab, 0, $number));
     }
 
-    static function generateIndex($html, $depth = 6)
-    {
-        //get the headings down to the specified depth
-        $pattern = '/<h[2-' . $depth . ']*[^>]*>.*?<\/h[2-' . $depth . ']>/';
-        $whocares = preg_match_all($pattern, $html, $winners);
 
-        //reformat the results to be more usable
-        $heads = implode("\n", $winners[0]);
-        $heads = str_replace('<a name="', '<a href="#', $heads);
-        $heads = str_replace('</a>', '', $heads);
-        $heads = preg_replace('/<h([1-' . $depth . '])>/', '<li class="toc$1">', $heads);
-        $heads = preg_replace('/<\/h[1-' . $depth . ']>/', '</a></li>', $heads);
-
-        //plug the results into appropriate HTML tags
-        $contents = '<div id="toc"> 
-	<p id="toc-header">Contents</p>
-	<ul>
-	' . $heads . '
-	</ul>
-	</div>';
-        return $contents;
-    }
 
     /**
      * It downloads all the images from a remote URL and saves them to a local directory
