4e235bcc6556ef2e7bba830708d11008158d9143
michael <m@cadot.eu>
2022-12-30 13:02:38 +0100

    maj

--- ArticleHelper.php
+++ ArticleHelper.php
@@ -10,7 +10,16 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class ArticleHelper
 {
-	/* Adding a table of contents to the article. */
+
+	/**
+	 * It takes an article, adds an ID to all the headers, and then generates a table of contents
+	 * 
+	 * @param article The article to add the Table of Contents to.
+	 * @param top The top level of the table of contents.
+	 * @param deep The number of levels of headings to include in the Table of Contents.
+	 * 
+	 * @return The article with the table of contents added to the top of the article.
+	 */
 	public static function addSommaire($article, $top = 1, $deep = 2)
 	{
 		if ($article == null) return $article;
@@ -19,13 +28,17 @@ class ArticleHelper
 		// This ensures that all header tags have `id` attributes so they can be used as anchor links
 		$article = $markupFixer->fix($article);
 		// This generates the Table of Contents in HTML
-		return "<div class='toc'>" .
-			$tocGenerator->getHtmlMenu($article, $top, $deep) .
-			'</div>' .
-			$article;
+		return "<div class='toc'>" . $tocGenerator->getHtmlMenu($article, $top, $deep) . '</div>' . $article;
 	}
 
-	/* Replacing the oembed tag with the youtube video. */
+	/**
+	 * It takes an article, finds all the oembed tags, replaces them with the YoutubeHelper::adaptUrl()
+	 * function, and returns the article
+	 * 
+	 * @param article the article to be parsed
+	 * 
+	 * @return The article with the oembed tags replaced with the YoutubeHelper::adaptUrl()
+	 */
 	public static function addLinkVideos($article)
 	{
 		$crawler = new Crawler($article);
@@ -42,11 +55,17 @@ class ArticleHelper
 		return $article;
 	}
 
-	/* Replacing the word "glossaire" with a span tag. */
-	public static function addLinkGlossaire(
-		$article,
-		GlossaireRepository $glossaireRepository
-	) {
+	/**
+	 * It takes an article and a glossary repository, and returns the article with the glossary terms
+	 * linked
+	 * 
+	 * @param article the article to be processed
+	 * @param GlossaireRepository glossaireRepository the repository of the glossary
+	 * 
+	 * @return A string with the glossary terms wrapped in a span tag.
+	 */
+	public static function addLinkGlossaire($article, GlossaireRepository $glossaireRepository)
+	{
 		$article = str_replace('&nbsp;', '', $article);
 		$mots = $glossaireRepository->findBy([
 			'deletedAt' => null,
@@ -72,69 +91,65 @@ class ArticleHelper
 		return $article;
 	}
 
-	public static function addFilterLiip(
-		$texte,
-		CacheManager $imagineCacheManager
-	) {
-		//on ajoute un filtre en fonction du champ style width
-		$crawler = new Crawler($texte);
-		//on vire les figures
-		foreach ($crawler->filter('figure') as $node) {
-			//@var node $node
-			$child = $node->getElementsByTagName('img');
-
-			// $child[0]->setAttribute('style', $node->getAttribute('style') . ' ' . $child[0]->getAttribute('style'));
-			// $child[0]->setAttribute('class', $node->getAttribute('class') . ' ' . $child[0]->getAttribute('class'));
-			// $node->removeAttribute('style');
-			// $node->removeAttribute('class');
-		}
-		//on vérifie aussi les img
-		// foreach ($crawler->filter('img') as $node) {
-		// 	$src = $node->getAttribute('src');
-		// 	foreach (explode(' ', $node->getAttribute('style')) as $style) {
-		// 		if (strpos($style, 'width') !== false) {
-		// 			$width = explode(':', $style)[1];
-		// 			$liip = str_replace(
-		// 				[
-		// 					';',
-		// 					'%',
-		// 					'0',
-		// 					'1',
-		// 					'2',
-		// 					'3',
-		// 					'4',
-		// 					'5',
-		// 					'6',
-		// 					'7',
-		// 					'8',
-		// 					'9',
-		// 					'.',
-		// 				],
-		// 				'',
-		// 				$width
-		// 			);
-		// 			$img = 'uploads/' . explode('uploads/', $src)[1];
-		// 			if ($liip) {
-		// 				dump('width:' . $width);
-		// 				$node->setAttribute(
-		// 					'style',
-		// 					str_replace(
-		// 						'width:' . $width,
-		// 						'',
-		// 						$node->getAttribute('style')
-		// 					)
-		// 				);
-		// 				$resolvedPath = $imagineCacheManager->getBrowserPath(
-		// 					$img,
-		// 					$liip
-		// 				);
-		// 				$node->setAttribute('src', $resolvedPath);
-		// 				//dump($resolvedPath);
-		// 			}
-		// 			//dump($resolvedPath);
-		// 		}
-		// 	}
-		// }
-		return $crawler->html();
-	}
+	// public static function addFilterLiip($texte, CacheManager $imagineCacheManager)
+	// {
+	// 	//on ajoute un filtre en fonction du champ liip
+	// 	$crawler = new Crawler($texte);
+	// 	foreach ($crawler->filter('img') as $node) {
+	// 		//@var node $node
+	// 		dd($node);
+	// 		// $child[0]->setAttribute('style', $node->getAttribute('liip') . ' ' . $child[0]->getAttribute('style'));
+	// 		// $child[0]->setAttribute('class', $node->getAttribute('class') . ' ' . $child[0]->getAttribute('class'));
+	// 		// $node->removeAttribute('style');
+	// 		// $node->removeAttribute('class');
+	// 	}
+	// 	//on vérifie aussi les img
+	// 	// foreach ($crawler->filter('img') as $node) {
+	// 	// 	$src = $node->getAttribute('src');
+	// 	// 	foreach (explode(' ', $node->getAttribute('style')) as $style) {
+	// 	// 		if (strpos($style, 'width') !== false) {
+	// 	// 			$width = explode(':', $style)[1];
+	// 	// 			$liip = str_replace(
+	// 	// 				[
+	// 	// 					';',
+	// 	// 					'%',
+	// 	// 					'0',
+	// 	// 					'1',
+	// 	// 					'2',
+	// 	// 					'3',
+	// 	// 					'4',
+	// 	// 					'5',
+	// 	// 					'6',
+	// 	// 					'7',
+	// 	// 					'8',
+	// 	// 					'9',
+	// 	// 					'.',
+	// 	// 				],
+	// 	// 				'',
+	// 	// 				$width
+	// 	// 			);
+	// 	// 			$img = 'uploads/' . explode('uploads/', $src)[1];
+	// 	// 			if ($liip) {
+	// 	// 				dump('width:' . $width);
+	// 	// 				$node->setAttribute(
+	// 	// 					'style',
+	// 	// 					str_replace(
+	// 	// 						'width:' . $width,
+	// 	// 						'',
+	// 	// 						$node->getAttribute('style')
+	// 	// 					)
+	// 	// 				);
+	// 	// 				$resolvedPath = $imagineCacheManager->getBrowserPath(
+	// 	// 					$img,
+	// 	// 					$liip
+	// 	// 				);
+	// 	// 				$node->setAttribute('src', $resolvedPath);
+	// 	// 				//dump($resolvedPath);
+	// 	// 			}
+	// 	// 			//dump($resolvedPath);
+	// 	// 		}
+	// 	// 	}
+	// 	// }
+	// 	return $crawler->html();
+	// }
 }
