b8f0b657f67135031f6d95711e30d682a2f95dfb
michael <m@cadot.eu>
2023-02-10 14:55:13 +0100

    ip et infos

--- IpHelper.php
+++ IpHelper.php
@@ -2,9 +2,16 @@
 
 namespace App\Service\base;
 
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class IpHelper
 {
+    private $httpClient;
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
     static public function getUserIP()
     {
 
@@ -15,5 +22,15 @@ class IpHelper
         } else {
             return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
         }
+    }
+    public function getIp()
+    {
+
+        return $this->httpClient->request('GET', 'https://ipecho.net/plain')->getContent();
+    }
+    public function getInformations()
+    {
+        $response = $this->httpClient->request('GET', 'http://ip-api.com/json/')->getContent();
+        return json_decode($response, true);
     }
 }
