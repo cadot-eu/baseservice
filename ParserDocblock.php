de53b30828b4b7c789ee51745b8c2b01809eaa2e
michael <michael@cadot.eu>
2023-09-02 19:33:19 +0200

    devis

--- ParserDocblock.php
+++ ParserDocblock.php
@@ -18,7 +18,7 @@ class ParserDocblock
         $baseAlias;
 
 
-    public function __construct(string $entity = '', array $baseAlias = ['integer', 'adresse', 'simple', 'simplelanguage', 'vide', 'normal', 'full', 'annonce', 'choice', 'choiceenplace', 'onechoiceenplace', 'entity', 'collection', 'color', 'email', 'password', 'hidden', 'hiddenroot', 'invisible', 'readonlyroot', 'image', 'video', 'fichier', 'money', 'telephone', 'siret', 'array', 'json', 'order', 'search', 'select', 'string', 'drapeau', 'pass', 'importance', 'stars', 'nombre'])
+    public function __construct(string $entity = '', array $baseAlias = ['integer', 'adresse', 'simple', 'simplelanguage', 'vide', 'normal', 'full', 'annonce', 'choice', 'choiceenplace', 'onechoiceenplace', 'entity', 'collection', 'color', 'email', 'password', 'hidden', 'hiddenroot', 'invisible', 'readonlyroot', 'image', 'video', 'fichier', 'money', 'telephone', 'siret', 'iban', 'bic', 'array', 'json', 'order', 'search', 'select', 'string', 'drapeau', 'pass', 'importance', 'stars', 'nombre'])
     {
         $this->setEntity($entity);
         $this->baseAlias = $baseAlias;
