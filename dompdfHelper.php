a2de40569dca17935de300a6953e4ef7e7cb1891
michael <michael@cadot.eu>
2023-09-16 17:49:58 +0200

    delete item

---
+++ dompdfHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service\base;
+
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Dompdf\Dompdf;
+
+class dompdfHelper
+{
+
+    /**
+     * The function converts HTML to PDF using the Dompdf library and streams the PDF file for download.
+     * 
+     * @param html The HTML content that you want to convert to a PDF.
+     * @param nameFichier The parameter "nameFichier" is used to specify the name of the PDF file that
+     * will be generated. If no name is provided, a unique identifier followed by the ".pdf" extension
+     * will be used as the file name.
+     * @param bool attachment The "attachment" parameter determines whether the PDF file should be
+     * downloaded as an attachment or displayed in the browser. If set to true, the PDF will be
+     * downloaded as an attachment. If set to false, the PDF will be displayed in the browser.
+     * 
+     * @return the result of the `stream()` method of the `` object.
+     */
+    public function toPdf($html, $nameFichier = null, bool $attachment = true)
+    {
+        if (!$nameFichier) {
+            $nameFichier = \uniqid() . '.pdf';
+        }
+        $dompdf = new Dompdf();
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+        return $dompdf->stream($nameFichier, ['Attachment' => $attachment]);
+    }
+}
